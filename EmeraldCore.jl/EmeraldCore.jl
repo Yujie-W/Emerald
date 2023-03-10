e344d3cb0a942cdc3da69202beb201ff102a5c26
Yujie Wang <jesiner@gmail.com>
2023-03-10 14:15:44 -0800

    add StomatalModel module

--- EmeraldCore.jl/EmeraldCore.jl
+++ EmeraldCore.jl/EmeraldCore.jl
@@ -11,12 +11,12 @@ include("EarthGeometry.jl"    )
 include("Optics.jl"           )
 include("PhysicalChemistry.jl")
 
-include("Soil.jl")
-
-include("CanopyOptics.jl"   )
 include("LeafOptics.jl"     )
+include("CanopyOptics.jl"   )
 include("Photosynthesis.jl" )
+include("Soil.jl"           )
 include("PlantHydraulics.jl")
+include("StomatalModels.jl" )
 
 
 end
---
+++ EmeraldCore.jl/StomatalModels.jl
@@ -0,0 +1,20 @@
+module StomatalModels
+
+using ..Constant: CP_D_MOL, K_STEFAN, M_H₂O
+using ..Namespace: AbstractSoilVC, AbstractXylemVC
+using ..Namespace: AbstractStomataModel, AndereggSM, BallBerrySM, BetaParameterG1, BetaParameterVcmax, EllerSM, GentineSM, LeuningSM, MedlynSM, SperrySM, WangSM, Wang2SM
+using ..Namespace: AirLayer, BroadbandLeafBiophysics, C4VJPModel, GCO₂Mode, HyperspectralLeafBiophysics, Leaf, LeafHydraulics, Leaves1D, Leaves2D
+using ..Namespace: MonoElementSPAC, MonoMLGrassSPAC, MonoMLPalmSPAC, MonoMLTreeSPAC
+using ..Photosynthesis: leaf_photosynthesis!, ∂R∂T
+using ..PhysicalChemistry: latent_heat_vapor, relative_diffusive_coefficient, relative_surface_tension, relative_viscosity, saturation_vapor_pressure
+using ..PlantHydraulics: relative_hydraulic_conductance, ∂E∂P
+using ..Soil: relative_hydraulic_conductance
+
+
+include("stomata/conductance.jl")
+include("stomata/empirical.jl"  )
+include("stomata/limits.jl"     )
+include("stomata/optimality.jl" )
+
+
+end # module
