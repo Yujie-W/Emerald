eb75a1487270238b30600d8afec3968f2e5d37dc
Yujie Wang <jesiner@gmail.com>
2023-04-26 16:29:44 -0700

    add functions for solar decl angle and day length

--- EmeraldCore.jl/EarthGeometry.jl
+++ EmeraldCore.jl/EarthGeometry.jl
@@ -7,6 +7,56 @@ using ..Constant: YEAR_D
 #
 # Changes to the function
 # General
+#     2023-Apr-26: add function daytime_length()
+#
+#######################################################################################################################################################################################################
+"""
+
+    day_length(lat::FT, fdoy::FT) where {FT<:AbstractFloat}
+
+Return the daytime length, given
+- `lat` Latitude in °
+- `fdoy` Day of year (digits after decimal for time of day)
+
+"""
+function day_length(lat::FT, fdoy::FT) where {FT<:AbstractFloat}
+    _deg  = 360 / YEAR_D(FT) * (fdoy + 10);
+    _decl = -FT(23.44) * cosd(_deg);
+
+    if -tand(lat) * tand(_decl) <= -1
+        return FT(24)
+    elseif -tand(lat) * tand(_decl) > 1
+        return FT(0)
+    else
+        return 2 * acosd(-tand(lat) * tand(_decl)) / 15
+    end
+end
+
+
+#######################################################################################################################################################################################################
+#
+# Changes to the function
+# General
+#     2023-Apr-26: add function solar_declination_angle
+#
+#######################################################################################################################################################################################################
+"""
+
+    solar_declination_angle(fdoy::FT) where {FT<:AbstractFloat}
+
+Return the solar declination angle, given
+- `fdoy` Day of year
+
+"""
+function solar_declination_angle(fdoy::FT) where {FT<:AbstractFloat}
+    return -FT(23.44) * cosd(360 / YEAR_D(FT) * (fdoy + 10))
+end
+
+
+#######################################################################################################################################################################################################
+#
+# Changes to the function
+# General
 #     2022-Sep-08: move function from SoilPlantAirContinuum.jl
 #     2022-Sep-08: make YEAR_D an option so that one can use ClimaCache value to replace it
 #     2022-Oct-19: use YEAR_D function from EmeraldConstants
@@ -37,16 +87,14 @@ solar_zenith_angle(lat::FT, decl::FT, lha::FT) where {FT<:AbstractFloat} = (
 );
 
 solar_zenith_angle(lat::FT, day::FT, hour::FT, minute::FT) where {FT<:AbstractFloat} = (
-    _deg  = 360 / YEAR_D(FT) * (day + (hour + minute / 60) / 24 + 10);
-    _decl = -FT(23.44) * cosd(_deg);
+    _decl = solar_declination_angle(day + (hour + minute / 60) / 24);
     _lha  = (hour - 12) * 15;
 
     return solar_zenith_angle(lat, _decl, _lha)
 );
 
 solar_zenith_angle(lat::FT, fdoy::FT) where {FT<:AbstractFloat} = (
-    _deg  = 360 / YEAR_D(FT) * (fdoy + 10);
-    _decl = -FT(23.44) * cosd(_deg);
+    _decl = solar_declination_angle(fdoy);
     _lha  = ((fdoy % 1) - FT(0.5)) * 360;
 
     return solar_zenith_angle(lat, _decl, _lha)
