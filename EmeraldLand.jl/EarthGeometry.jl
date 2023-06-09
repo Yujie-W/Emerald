55ba894cc3b358bf818aca596670a2128b4503d1
Yujie Wang <jesiner@gmail.com>
2023-06-09 10:51:04 -0700

    add function for solar azimuth angle

--- EmeraldLand.jl/EarthGeometry.jl
+++ EmeraldLand.jl/EarthGeometry.jl
@@ -37,6 +37,59 @@ end
 #
 # Changes to the function
 # General
+#     2023-Jun-09: add function solar_azimuth_angle
+#
+#######################################################################################################################################################################################################
+"""
+
+    solar_azimuth_angle(lat::FT, decl::FT, lha::FT) where {FT<:AbstractFloat}
+    solar_azimuth_angle(lat::FT, day::FT, hour::FT, minute::FT) where {FT<:AbstractFloat}
+    solar_azimuth_angle(lat::FT, fdoy::FT) where {FT<:AbstractFloat}
+
+Return the solar azimuth angle, given
+- `lat` Latitude in °
+- `decl` Declination of the Sun in °
+- `lha` Local hour angle in °
+- `day` Day of year
+- `hour` Hour of day
+- `minute` Minute of hour
+- `fdoy` Day of year (digits after decimal for time of day)
+
+"""
+function solar_azimuth_angle end
+
+solar_azimuth_angle(lat::FT, decl::FT, lha::FT) where {FT<:AbstractFloat} = (
+    _cos_sza = sind(lat) * sind(decl) + cosd(lat) * cosd(decl) * cosd(lha);
+
+    if _cos_sza == 1
+        return lat >= 0 ? FT(180) : FT(0)
+    end;
+
+    _sin_sza = sqrt(1 - _cos_sza^2);
+    _cos_saa = (sind(decl) - sind(lat) * _cos_sza) / (cosd(lat) * _sin_sza);
+
+    return lha >= 0 ? 360 - acosd(_cos_saa) : acosd(_cos_saa)
+);
+
+solar_azimuth_angle(lat::FT, day::FT, hour::FT, minute::FT) where {FT<:AbstractFloat} = (
+    _decl = solar_declination_angle(day + (hour + minute / 60) / 24);
+    _lha  = (hour - 12) * 15;
+
+    return solar_azimuth_angle(lat, _decl, _lha)
+);
+
+solar_azimuth_angle(lat::FT, fdoy::FT) where {FT<:AbstractFloat} = (
+    _decl = solar_declination_angle(fdoy);
+    _lha  = ((fdoy % 1) - FT(0.5)) * 360;
+
+    return solar_azimuth_angle(lat, _decl, _lha)
+);
+
+
+#######################################################################################################################################################################################################
+#
+# Changes to the function
+# General
 #     2023-Apr-26: add function solar_declination_angle
 #
 #######################################################################################################################################################################################################
