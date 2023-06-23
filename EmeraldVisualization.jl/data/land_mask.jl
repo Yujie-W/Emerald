e707ec85b970dc84c0fc2a65b40a5ecbdd01e5fc
Yujie Wang <jesiner@gmail.com>
2023-06-23 13:46:34 -0700

    mask data for 2d and 3d data

--- EmeraldVisualization.jl/data/land_mask.jl
+++ EmeraldVisualization.jl/data/land_mask.jl
@@ -17,6 +17,10 @@ Filter out NaNs in data based on land mask, given
 
 """
 function land_mask_data!(data::Array{FT,N}; division::Int = 1, replacement::Number = 0, threshold::Number = 0.01) where {FT,N}
+    @assert N in [2, 3] "data must be 2D or 3D";
+    @assert size(data,1) == division * 360 "data must be 360 * division in longitude";
+    @assert size(data,2) == division * 180 "data must be 180 * division in latitude";
+
     # read land mask data from GriddingMachine if the data is not yet loaded into memory
     if isnothing(LAND_MASK)
         global LAND_MASK = read_LUT(query_collection("LM_4X_1Y_V1"))[1];
@@ -26,10 +30,16 @@ function land_mask_data!(data::Array{FT,N}; division::Int = 1, replacement::Numb
     _land_mask = regrid(LAND_MASK, division);
 
     # filter out NaNs in data based on land mask threshold
-    _nan_mask = isnan.(data) .&& (_land_mask .>= threshold);
-
     # replace NaNs in data with replacement
-    data[_nan_mask] .= replacement;
+    if N == 2
+        _nan_mask = isnan.(data) .&& (_land_mask .>= threshold);
+        data[_nan_mask] .= replacement;
+    elseif N == 3
+        for _i in axes(data, 3)
+            _nan_mask = isnan.(data[:,:,_i]) .&& (_land_mask .>= threshold);
+            data[_nan_mask, _i] .= replacement;
+        end;
+    end;
 
     return nothing
 end
