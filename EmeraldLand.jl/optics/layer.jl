2614fe914062f58ba2f3dd795c83523758edc2a4
Yujie Wang <jesiner@gmail.com>
2023-09-14 22:27:46 -0700

    leaf optics: add inhomogenoues layer rho and tau

--- EmeraldLand.jl/optics/layer.jl
+++ EmeraldLand.jl/optics/layer.jl
@@ -80,6 +80,155 @@ layer_ρ_τ(lha::HyperspectralAbsorption{FT}, θ::FT) where {FT} = (
 #
 # Changes to this function
 # General
+#     2023-Sep-14: add function to compute reflectance and transmittance of the single layer of a leaf with non isotropic radiation
+#
+#######################################################################################################################################################################################################
+"""
+
+    layer_ρ_τ_direct(lha::HyperspectralAbsorption{FT}, bio::HyperspectralLeafBiophysics{FT}, lwc::FT, θ::FT; N::Int = 10) where {FT}
+
+Return the reflectance and transmittance of the single layer of a leaf with non isotropic radiation, given
+- `lha` leaf hyperspectral absorption coefficients
+- `bio` leaf hyperspectral biophysics
+- `lwc` leaf water content
+- `θ` average incident angle of the incoming radiation
+- `N` number of sublayers of the each sublayer (default: 10)
+
+"""
+function layer_ρ_τ_direct(lha::HyperspectralAbsorption{FT}, bio::HyperspectralLeafBiophysics{FT}, lwc::FT, θ::FT; N::Int = 10) where {FT}
+    (; NR) = lha;
+    (; MESOPHYLL_N) = bio;
+
+    # radiation beam from the surface (100%)
+    _τ_surface = interface_isotropic_τ.(FT(1), NR, θ);
+    _ρ_surface = 1 .- _τ_surface;
+
+    # scatter light from water to air (100%)
+    _τ_21 = interface_isotropic_τ.(NR, FT(1), FT(90));
+    _ρ_21 = 1 .- _τ_21;
+
+    # absorption ratio within the layer
+    # this one should be similar to sublayer_transmittance(lha, bio, x, 1)
+    _t_all = sublayer_τ(lha, bio, lwc, 1/MESOPHYLL_N, N) .^ N;
+
+    # compute the integrated reflectance
+    _denom = 1 .- _ρ_21 .* _t_all .* _ρ_21 .* _t_all;
+    _ρ_int = _ρ_surface .+ _τ_surface .* _t_all .* _ρ_21 .* _t_all .* _τ_21 ./ _denom;
+    _τ_int = _τ_surface .* _t_all .* _τ_21 ./ _denom;
+
+    # You can double check the conservation like this
+    # _α_int = _τ_surface .* (1 .- _t_all) ./ (1 .- _t_all .* _ρ_21);
+
+    return _ρ_int, _τ_int
+end;
+
+
+#######################################################################################################################################################################################################
+#
+# Changes to this function
+# General
+#     2023-Sep-14: add function to compute reflectance and transmittance of the single layer of a leaf with diffuse radiation
+#
+#######################################################################################################################################################################################################
+"""
+
+    layer_ρ_τ_diffuse(lha::HyperspectralAbsorption{FT}, bio::HyperspectralLeafBiophysics{FT}, lwc::FT; N::Int = 10) where {FT}
+    layer_ρ_τ_diffuse(lha::HyperspectralAbsorption{FT}, bio::HyperspectralLeafBiophysics{FT}) where {FT}
+
+Return the reflectance and transmittance of the single layer of a leaf with diffuse radiation (if lwc and N not provided, return the values of the case with zero absorption), given
+- `lha` leaf hyperspectral absorption coefficients
+- `bio` leaf hyperspectral biophysics
+- `lwc` leaf water content
+- `N` number of sublayers of the each sublayer (default: 10)
+
+"""
+function layer_ρ_τ_diffuse end;
+
+layer_ρ_τ_diffuse(lha::HyperspectralAbsorption{FT}, bio::HyperspectralLeafBiophysics{FT}, lwc::FT; N::Int = 10) where {FT} = (
+    (; NR) = lha;
+    (; MESOPHYLL_N) = bio;
+
+    # radiation beam from the surface (100%)
+    _τ_surface = interface_isotropic_τ.(FT(1), NR, FT(90));
+    _ρ_surface = 1 .- _τ_surface;
+
+    # scatter light from water to air (100%)
+    _τ_21 = interface_isotropic_τ.(NR, FT(1), FT(90));
+    _ρ_21 = 1 .- _τ_21;
+
+    # absorption ratio within the layer
+    # this one should be similar to sublayer_transmittance(lha, bio, x, 1)
+    _t_all = sublayer_τ(lha, bio, lwc, 1/MESOPHYLL_N, N) .^ N;
+
+    # compute the integrated reflectance for the first layer
+    _ρ_1 = similar(_t_all);
+    _τ_1 = similar(_t_all);
+    _ρ_2 = similar(_t_all);
+    _τ_2 = similar(_t_all);
+    _denom = similar(_t_all);
+    @. _denom = 1 - _ρ_21 * _t_all * _ρ_21 * _t_all;
+    @. _ρ_1 = _ρ_surface + _τ_surface * _t_all * _ρ_21 * _t_all * _τ_21 / _denom;
+    @. _τ_1 = _τ_surface * _t_all * _τ_21 / _denom;
+
+    # compute the integrated reflectance and transmittance for the n-1 layer
+    d = similar(_t_all);
+    a = similar(_t_all);
+    b = similar(_t_all);
+    @. d = sqrt((_τ_1 ^ 2 - _ρ_1 ^ 2 - 1) ^ 2 - 4 * _ρ_1 ^ 2);
+    @. a = (1 + _ρ_1 ^ 2 - _τ_1 ^ 2 + d) / (2 * _ρ_1);
+    @. b = (1 - _ρ_1 ^ 2 + _τ_1 ^ 2 + d) / (2 * _τ_1);
+    m = MESOPHYLL_N - 1;
+    @. _ρ_2 = (b ^ m - 1 / (b ^ m)) / (a * b ^ m - 1 / (a * b ^ m));
+    @. _τ_2 = (a - 1 / a) / (a * b ^ m - 1 / (a * b ^ m));
+
+    return _ρ_1, _τ_1, _ρ_2, _τ_2
+);
+
+layer_ρ_τ_diffuse(lha::HyperspectralAbsorption{FT}, bio::HyperspectralLeafBiophysics{FT}) where {FT} = (
+    (; NR) = lha;
+    (; MESOPHYLL_N) = bio;
+
+    # radiation beam from the surface (100%)
+    _τ_surface = interface_isotropic_τ.(FT(1), NR, FT(90));
+    _ρ_surface = 1 .- _τ_surface;
+
+    # scatter light from water to air (100%)
+    _τ_21 = interface_isotropic_τ.(NR, FT(1), FT(90));
+    _ρ_21 = 1 .- _τ_21;
+
+    # absorption ratio within the layer
+    # this one should be similar to sublayer_transmittance(lha, bio, x, 1)
+    _t_all = ones(length(NR));
+
+    # compute the integrated reflectance for the first layer
+    _ρ_1 = similar(_t_all);
+    _τ_1 = similar(_t_all);
+    _ρ_2 = similar(_t_all);
+    _τ_2 = similar(_t_all);
+    _denom = similar(_t_all);
+    @. _denom = 1 - _ρ_21 * _t_all * _ρ_21 * _t_all;
+    @. _ρ_1 = _ρ_surface + _τ_surface * _t_all * _ρ_21 * _t_all * _τ_21 / _denom;
+    @. _τ_1 = _τ_surface * _t_all * _τ_21 / _denom;
+
+    # compute the integrated reflectance and transmittance for the n-1 layer (d = 0, a = 1, b = 1 here after solved analytically)
+    #     solve this:
+    #         tau(n) = tau(1) * tau(n-1) / (1 - rho(1) * rho(n-1))                  =>
+    #         tau(n) = tau(1) * tau(n-1) / (tau(1) + tau(n-1) - tau(1) * tau(n-1))  =>
+    # according to wolframalpha.com
+    #         tau(n) = tau(1) / (tau(1) + n * tau(1))
+    #
+    m = MESOPHYLL_N - 1;
+    @. _τ_2 = _τ_1 / (_τ_1 + m - m * _τ_1);
+    @. _ρ_2 = 1 - _τ_2;
+
+    return _ρ_1, _τ_1, _ρ_2, _τ_2
+);
+
+
+#######################################################################################################################################################################################################
+#
+# Changes to this function
+# General
 #     2023-Sep-14: add function to compute the SIF excitation ratio of a single layer of a leaf
 # To do
 #     TODO: this function is not used, consider to remove it later
