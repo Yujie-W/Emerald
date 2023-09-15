f5ffafa10a6167436e977561609498afbe69a044
Yujie Wang <jesiner@gmail.com>
2023-09-14 22:30:06 -0700

    leaf optics: inhomogeneous layers in leaf spectra function

--- EmeraldLand.jl/optics/leaf.jl
+++ EmeraldLand.jl/optics/leaf.jl
@@ -3,6 +3,7 @@
 # Changes to this function
 # General
 #     2023-Sep-14: add function to compute the leaf spectra for reflectance and transmittance
+#     2023-Sep-14: use layer_ρ_τ_direct and layer_ρ_τ_diffuse to compute the reflectance and transmittance of the leaf layers
 #
 #######################################################################################################################################################################################################
 """
@@ -18,10 +19,9 @@ Return the leaf spectra for reflectance and transmittance, given
 
 """
 function leaf_spectra(lha::HyperspectralAbsorption{FT}, bio::HyperspectralLeafBiophysics{FT}, lwc::FT, θ::FT = FT(40); N::Int = 10) where {FT}
-    (; MESOPHYLL_N) = bio;
-
     # compute the reflectance and transmittance of the adaxial and abaixal surfaces (with optimum angle)
-    _ρ_s,_τ_s = layer_ρ_τ(lha, bio, lwc, 1/MESOPHYLL_N, θ; N= N);
+    # _ρ_s,_τ_s = layer_ρ_τ(lha, bio, lwc, 1/MESOPHYLL_N, θ; N= N);
+    _ρ_s,_τ_s = layer_ρ_τ_direct(lha, bio, lwc, θ; N= N);
 
     # define the vectors
     _denom = similar(_ρ_s);
@@ -29,8 +29,9 @@ function leaf_spectra(lha::HyperspectralAbsorption{FT}, bio::HyperspectralLeafBi
     _τ_leaf = similar(_ρ_s);
 
     # compute the reflectance and transmittance of the leaf layers (isotropic light)
-    _ρ_1,_τ_1 = layer_ρ_τ(lha, bio, lwc, 1/MESOPHYLL_N, FT(90); N = N);
-    _ρ_2,_τ_2 = layer_ρ_τ(lha, bio, lwc, 1 - 1/MESOPHYLL_N, FT(90); N = N);
+    # _ρ_1,_τ_1 = layer_ρ_τ(lha, bio, lwc, 1/MESOPHYLL_N, FT(90); N = N);
+    # _ρ_2,_τ_2 = layer_ρ_τ(lha, bio, lwc, 1 - 1/MESOPHYLL_N, FT(90); N = N);
+    _ρ_1,_τ_1,_ρ_2,_τ_2 = layer_ρ_τ_diffuse(lha, bio, lwc; N = N);
 
     @. _denom = 1 - _ρ_1 * _ρ_2;
     @. _τ_leaf = _τ_s * _τ_2 / _denom;
@@ -51,6 +52,7 @@ end;
 # Changes to this function
 # General
 #     2023-Sep-14: add function to compute the leaf level SIF matrices (before any reabsorption)
+#     2023-Sep-14: use layer_ρ_τ_direct and layer_ρ_τ_diffuse to compute the reflectance and transmittance of the leaf layers
 #
 #######################################################################################################################################################################################################
 """
@@ -69,12 +71,13 @@ Return the leaf level SIF matrices (before any reabsorption), given
 function leaf_raw_sif_matrices(lha::HyperspectralAbsorption{FT}, wls::WaveLengthSet{FT}, bio::HyperspectralLeafBiophysics{FT}, lwc::FT, θ::FT = FT(40); N::Int = 10) where {FT}
     (; Φ_PS) = lha;
     (; IΛ_SIF, IΛ_SIFE) = wls;
-    (; MESOPHYLL_N) = bio;
 
     # compute the reflectance and transmittance of the leaf layers (isotropic light)
-    _ρ_s,_τ_s = layer_ρ_τ(lha, bio, lwc, 1/MESOPHYLL_N, θ; N = N);
-    _ρ_1,_τ_1 = layer_ρ_τ(lha, bio, lwc, 1/MESOPHYLL_N, FT(90); N = N);
-    _ρ_2,_τ_2 = layer_ρ_τ(lha, bio, lwc, 1 - 1/MESOPHYLL_N, FT(90); N = N);
+    # _ρ_s,_τ_s = layer_ρ_τ(lha, bio, lwc, 1/MESOPHYLL_N, θ; N = N);
+    # _ρ_1,_τ_1 = layer_ρ_τ(lha, bio, lwc, 1/MESOPHYLL_N, FT(90); N = N);
+    # _ρ_2,_τ_2 = layer_ρ_τ(lha, bio, lwc, 1 - 1/MESOPHYLL_N, FT(90); N = N);
+    _ρ_s,_τ_s = layer_ρ_τ_direct(lha, bio, lwc, θ; N= N);
+    _ρ_1,_τ_1,_ρ_2,_τ_2 = layer_ρ_τ_diffuse(lha, bio, lwc; N = N);
     _denom = 1 .- _ρ_1 .* _ρ_2;
 
     # compute the mat_b_chl and mat_f_chl of the up and lower layers of a 2-layer leaf
@@ -97,12 +100,13 @@ function leaf_raw_sif_matrices(lha::HyperspectralAbsorption{FT}, wls::WaveLength
     _mat_f_2 = _sife_2 ./ 2 * Φ_PS[IΛ_SIF]';
 
     # here use the reflectance of a layer without absorption
-    _ρ_zero,_τ_zero = layer_ρ_τ(lha, FT(90));
-    _denom = 1 .- _ρ_zero .^ 2;
+    #_ρ_zero,_τ_zero = layer_ρ_τ(lha, FT(90));
+    _ρ0_1,_τ0_1,_ρ0_2,_τ0_2 = layer_ρ_τ_diffuse(lha, bio);
+    _denom = 1 .- _ρ0_1 .* _ρ0_2;
 
     # now consider the reflectance of SIF between the two layers to compute the matrices for the entire leaf (forward and backward)
-    _mat_b = _mat_b_1 .+ _mat_f_1 .* _ρ_zero[IΛ_SIF]' .* _τ_zero[IΛ_SIF]' ./ _denom[IΛ_SIF]' .+ _mat_b_2 .* _τ_zero[IΛ_SIF]' ./ _denom[IΛ_SIF]';
-    _mat_f = _mat_f_1 .* _τ_zero[IΛ_SIF]' ./ _denom[IΛ_SIF]' .+ _mat_f_2 .+ _mat_b_2 .* _ρ_zero[IΛ_SIF]' .* _τ_zero[IΛ_SIF]' ./ _denom[IΛ_SIF]';
+    _mat_b = _mat_b_1 .+ _mat_f_1 .* _ρ0_2[IΛ_SIF]' .* _τ0_1[IΛ_SIF]' ./ _denom[IΛ_SIF]' .+ _mat_b_2 .* _τ0_1[IΛ_SIF]' ./ _denom[IΛ_SIF]';
+    _mat_f = _mat_f_1 .* _τ0_2[IΛ_SIF]' ./ _denom[IΛ_SIF]' .+ _mat_f_2 .+ _mat_b_2 .* _ρ0_1[IΛ_SIF]' .* _τ0_2[IΛ_SIF]' ./ _denom[IΛ_SIF]';
 
     # the matrices should be conserved
     # @show sum(_sife_1 .+ _sife_2);
@@ -118,6 +122,7 @@ end;
 # Changes to this function
 # General
 #     2023-Sep-14: add function to compute the leaf level SIF matrices (after leaf reabsorption)
+#     2023-Sep-14: use layer_ρ_τ_direct and layer_ρ_τ_diffuse to compute the reflectance and transmittance of the leaf layers
 #
 #######################################################################################################################################################################################################
 """
@@ -142,12 +147,13 @@ function leaf_sif_matrices(lha::HyperspectralAbsorption{FT}, wls::WaveLengthSet{
     _τ_sf_sife = interface_isotropic_τ.(FT(1), NR[IΛ_SIFE], θ);
     _τ_21_sife = interface_isotropic_τ.(NR[IΛ_SIFE], FT(1), FT(90));
     _ρ_21_sife = 1 .- _τ_21_sife;
-    _τ_12_sife = interface_isotropic_τ.(FT(1), NR[IΛ_SIFE], FT(90));
 
     # reflectance and transmittance through the layer
-    _,_τ_0 = layer_ρ_τ(lha, bio, lwc, 1/MESOPHYLL_N, θ; N = N);
-    _ρ_1,_τ_1 = layer_ρ_τ(lha, bio, lwc, 1/MESOPHYLL_N, FT(90); N = N);
-    _ρ_2,_τ_2 = layer_ρ_τ(lha, bio, lwc, 1 - 1/MESOPHYLL_N, FT(90); N = N);
+    # _,_τ_0 = layer_ρ_τ(lha, bio, lwc, 1/MESOPHYLL_N, θ; N = N);
+    # _ρ_1,_τ_1 = layer_ρ_τ(lha, bio, lwc, 1/MESOPHYLL_N, FT(90); N = N);
+    # _ρ_2,_τ_2 = layer_ρ_τ(lha, bio, lwc, 1 - 1/MESOPHYLL_N, FT(90); N = N);
+    _,_τ_0 = layer_ρ_τ_direct(lha, bio, lwc, θ; N= N);
+    _ρ_1,_τ_1,_ρ_2,_τ_2 = layer_ρ_τ_diffuse(lha, bio, lwc; N = N);
 
     #
     # PART 1: compute the matrices for the upper layer
@@ -195,8 +201,13 @@ function leaf_sif_matrices(lha::HyperspectralAbsorption{FT}, wls::WaveLengthSet{
     _t_all_sife = _t_sub_sife .^ N;
     _t_sub_sif  = _t_sub[IΛ_SIF];
 
-    # 2. compute SIF emissions at the SIFE absorption site and rescale it based on the SIF absorption
-    _rad_i = _τ_0[IΛ_SIFE] .* _τ_12_sife;
+    # 2. compute the effective τ_21 from the effective reflectance and transmittance of the n-1 layers
+    _τ_21_rescale = (_t_all_sife .* _τ_2[IΛ_SIFE] .+ 1 .- _ρ_2[IΛ_SIFE]) ./ (_t_all_sife .* _τ_2[IΛ_SIFE] .+ NR[IΛ_SIFE]);
+    _τ_12_rescale = _τ_21_rescale .* NR[IΛ_SIFE];
+    _ρ_21_rescale = 1 .- _τ_21_rescale;
+
+    # 3. compute SIF emissions at the SIFE absorption site and rescale it based on the SIF absorption
+    _rad_i = _τ_0[IΛ_SIFE] .* _τ_12_rescale;
     _mat_b_2 = zeros(FT, length(IΛ_SIFE), length(IΛ_SIF));
     _mat_f_2 = zeros(FT, length(IΛ_SIFE), length(IΛ_SIF));
     # now the radiation goes from up to down
@@ -206,14 +217,14 @@ function leaf_sif_matrices(lha::HyperspectralAbsorption{FT}, wls::WaveLengthSet{
         _rad_i .*= _t_sub_sife;
     end;
     # then the radiation goes from down to up
-    _rad_i .*= _ρ_21_sife;
+    _rad_i .*= _ρ_21_rescale;
     for _i in N:-1:1
         _mat_b_2 .+= (_rad_i ./ 2 .* _a_sub_sife) * Φ_PS[IΛ_SIF]' .* (_t_sub_sif .^ (_i -0.5))';
         _mat_f_2 .+= (_rad_i ./ 2 .* _a_sub_sife) * Φ_PS[IΛ_SIF]' .* (_t_sub_sif .^(N - _i + 0.5))';
         _rad_i .*= _t_sub_sife;
     end;
     # then rescale it by 1 / (1 - ρ_21 * t_all * ρ_21 * t_all) for the SIFE
-    _denom = 1 .- _t_all_sife .* _ρ_21_sife .* _t_all_sife .* _ρ_21_sife;
+    _denom = 1 .- _t_all_sife .* _ρ_21_rescale .* _t_all_sife .* _ρ_21_rescale;
     _mat_b_2 ./= _denom;
     _mat_f_2 ./= _denom;
 
