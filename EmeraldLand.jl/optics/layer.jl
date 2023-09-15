0e8cd857cf6c0060860055e8361a437c92058a01
Yujie Wang <jesiner@gmail.com>
2023-09-15 14:31:24 -0700

    leaf optics: add functions to update layer tau and rho

--- EmeraldLand.jl/optics/layer.jl
+++ EmeraldLand.jl/optics/layer.jl
@@ -18,7 +18,7 @@ Return the reflectance and transmittance of the single layer of a leaf (if bio, 
 - `lwc` leaf water content
 - `x` proportion of the layer of the whole leaf
 - `θ` average incident angle of the incoming radiation
-- `N` number of sublayers of the each sublayer (default: 10)
+- `N` number of sublayers of each layer
 
 """
 function layer_ρ_τ end;
@@ -92,7 +92,7 @@ Return the reflectance and transmittance of the single layer of a leaf with non 
 - `bio` leaf hyperspectral biophysics
 - `lwc` leaf water content
 - `θ` average incident angle of the incoming radiation
-- `N` number of sublayers of the each sublayer (default: 10)
+- `N` number of sublayers of each layer
 
 """
 function layer_ρ_τ_direct(lha::HyperspectralAbsorption{FT}, bio::HyperspectralLeafBiophysics{FT}, lwc::FT, θ::FT; N::Int = 10) where {FT}
@@ -122,6 +122,150 @@ function layer_ρ_τ_direct(lha::HyperspectralAbsorption{FT}, bio::Hyperspectral
     return _ρ_int, _τ_int
 end;
 
+#######################################################################################################################################################################################################
+#
+# Changes to this function
+# General
+#     2023-Sep-15: add function to compute the rescaled reflectance of the first layer of a leaf
+#
+#######################################################################################################################################################################################################
+"""
+
+    layer_1_ρ(τ_in::FT, ρ_21::FT, τ_sub::FT, N::Int) where {FT}
+
+Return the rescaled reflectance of the first layer of a leaf, given
+- `τ_in` transmittance of the incoming radiation
+- `ρ_21` reflectance at the water(2)-air(1) interface
+- `τ_sub` transmittance within a sublayer
+- `N` number of sublayers of each layer
+
+"""
+function layer_1_ρ(τ_in::FT, ρ_21::FT, τ_sub::FT, N::Int) where {FT}
+    τ_all = τ_sub ^ N;
+    denom = 1 - ρ_21 * τ_all * ρ_21 * τ_all;
+
+    return 1 - τ_in + τ_in * τ_all * ρ_21 * τ_all * (1 - ρ_21) / denom
+end;
+
+
+#######################################################################################################################################################################################################
+#
+# Changes to this function
+# General
+#     2023-Sep-15: add function to compute the rescaled transmittance of the first layer of a leaf
+#
+#######################################################################################################################################################################################################
+"""
+
+    layer_1_τ(τ_in::FT, ρ_21::FT, τ_sub::FT, N::Int) where {FT}
+
+Return the rescaled transmittance of the first layer of a leaf, given
+- `τ_in` transmittance of the incoming radiation
+- `ρ_21` reflectance at the water(2)-air(1) interface
+- `τ_sub` transmittance within a sublayer
+- `N` number of sublayers of each layer
+
+"""
+function layer_1_τ(τ_in::FT, ρ_21::FT, τ_sub::FT, N::Int) where {FT}
+    τ_all = τ_sub ^ N;
+    denom = 1 - ρ_21 * τ_all * ρ_21 * τ_all;
+
+    return τ_in * τ_all * (1 - ρ_21) / denom
+end;
+
+
+#######################################################################################################################################################################################################
+#
+# Changes to this function
+# General
+#     2023-Sep-15: add function to compute the rescaled reflectance of the n-1 layer of a leaf
+#
+# Note, when there is no absorption, d = 0, a = 1, and b = 1 here.
+# Then the problem to compute the integrated reflectance and transmittance for the n-1 layer is to solve
+#     tau(n) = tau(1) * tau(n-1) / (1 - rho(1) * rho(n-1))                  =>
+#     tau(n) = tau(1) * tau(n-1) / (tau(1) + tau(n-1) - tau(1) * tau(n-1))  =>
+# according to wolframalpha.com
+#      tau(n) = tau(1) / (tau(1) + n * tau(1))
+#
+#######################################################################################################################################################################################################
+"""
+
+    layer_2_ρ(ρ₁::FT, τ₁::FT, m::FT) where {FT}
+
+Return the rescaled reflectance of the n-1 layer of a leaf, given
+- `ρ₁` reflectance of the first layer
+- `τ₁` transmittance of the first layer
+- `m` proportion of the n-1 layers of the whole leaf
+
+"""
+function layer_2_ρ(ρ₁::FT, τ₁::FT, m::FT) where {FT}
+    ρ₁² = ρ₁ ^ 2;
+    τ₁² = τ₁ ^ 2;
+    d = sqrt((τ₁² - ρ₁² - 1) ^ 2 - 4ρ₁²);
+    a = (1 + ρ₁² - τ₁² + d) / 2ρ₁;
+    b = (1 - ρ₁² + τ₁² + d) / 2τ₁;
+
+    return (b ^ m - 1 / (b ^ m)) / (a * b ^ m - 1 / (a * b ^ m))
+end;
+
+
+#######################################################################################################################################################################################################
+#
+# Changes to this function
+# General
+#     2023-Sep-15: add function to compute the rescaled transmittance of the n-1 layer of a leaf
+#
+#######################################################################################################################################################################################################
+"""
+
+    layer_2_τ(ρ₁::FT, τ₁::FT, m::FT) where {FT}
+
+Return the rescaled reflectance of the n-1 layer of a leaf, given
+- `ρ₁` reflectance of the first layer
+- `τ₁` transmittance of the first layer
+- `m` proportion of the n-1 layers of the whole leaf
+
+"""
+function layer_2_τ(ρ₁::FT, τ₁::FT, m::FT) where {FT}
+    ρ₁² = ρ₁ ^ 2;
+    τ₁² = τ₁ ^ 2;
+    d = sqrt((τ₁² - ρ₁² - 1) ^ 2 - 4ρ₁²);
+    a = (1 + ρ₁² - τ₁² + d) / 2ρ₁;
+    b = (1 - ρ₁² + τ₁² + d) / 2τ₁;
+
+    return (a - 1 / a) / (a * b ^ m - 1 / (a * b ^ m))
+end;
+
+
+#######################################################################################################################################################################################################
+#
+# Changes to this function
+# General
+#     2023-Sep-15: add function to update the reflectance and transmittance of the leaf layers
+#
+#######################################################################################################################################################################################################
+"""
+
+    leaf_layer_spectra!(bio::HyperLeafBio{FT}, N::Int) where {FT}
+
+Update the reflectance and transmittance of the leaf layers, given
+- `bio` leaf hyperspectral biophysics
+- `N` number of sublayers of each layer
+
+"""
+function leaf_layer_spectra!(bio::HyperLeafBio{FT}, N::Int) where {FT}
+    bio.auxil.ρ_layer_θ .= layer_1_ρ.(bio.auxil.τ_interface_θ , bio.auxil.ρ_21, bio.auxil.τ_sub_1, N);
+    bio.auxil.τ_layer_θ .= layer_1_τ.(bio.auxil.τ_interface_θ , bio.auxil.ρ_21, bio.auxil.τ_sub_1, N);
+    bio.auxil.ρ_layer_1 .= layer_1_ρ.(bio.auxil.τ_interface_12, bio.auxil.ρ_21, bio.auxil.τ_sub_1, N);
+    bio.auxil.τ_layer_1 .= layer_1_τ.(bio.auxil.τ_interface_12, bio.auxil.ρ_21, bio.auxil.τ_sub_1, N);
+
+    x = 1 - 1 / bio.state.meso_n;
+    bio.auxil.ρ_layer_2 .= layer_2_ρ.(bio.auxil.ρ_layer_1, bio.auxil.τ_layer_1, x, N);
+    bio.auxil.τ_layer_2 .= layer_2_τ.(bio.auxil.ρ_layer_1, bio.auxil.τ_layer_1, x, N);
+
+    return nothing
+end;
+
 
 #######################################################################################################################################################################################################
 #
@@ -139,7 +283,7 @@ Return the reflectance and transmittance of the single layer of a leaf with diff
 - `lha` leaf hyperspectral absorption coefficients
 - `bio` leaf hyperspectral biophysics
 - `lwc` leaf water content
-- `N` number of sublayers of the each sublayer (default: 10)
+- `N` number of sublayers of each layer
 
 """
 function layer_ρ_τ_diffuse end;
@@ -245,7 +389,7 @@ Return the SIF excitation ratio of a single layer of a leaf, given
 - `lwc` leaf water content
 - `x` proportion of the layer of the whole leaf
 - `θ` average incident angle of the incoming radiation
-- `N` number of sublayers of the each layer (default: 10)
+- `N` number of sublayers of each layer
 
 """
 function layer_raw_sife(lha::HyperspectralAbsorption{FT}, wls::WaveLengthSet{FT}, bio::HyperspectralLeafBiophysics{FT}, lwc::FT, x::FT, θ::FT; N::Int = 10) where {FT}
--- EmeraldLand.jl/optics/sublayer.jl
+++ EmeraldLand.jl/optics/sublayer.jl
@@ -1,64 +1,5 @@
 #######################################################################################################################################################################################################
 #
-# Changes to the function
-# General
-#     2023-Sep-14: add function to compute the transmittance of a sublayer of a leaf with pigments
-#     2023-Sep-14: compute the total τ for N layers and then downscale it to τ^(1/N)
-#     2023-Sep-14: compute the fraction of cab and car absorption in the sublayer
-#
-#######################################################################################################################################################################################################
-"""
-
-    sublayer_τ(lha::HyperspectralAbsorption{FT}, bio::HyperspectralLeafBiophysics{FT}, lwc::FT, x::FT, N::Int) where {FT}
-
-Return the transmittance of a sublayer of a leaf with pigments, given
-- `lha` leaf hyperspectral absorption coefficients
-- `bio` leaf hyperspectral biophysics
-- `lwc` leaf water content
-- `x` proportion of the layer of the whole leaf
-- `N` number of sublayers of the whole layer
-
-"""
-function sublayer_τ(lha::HyperspectralAbsorption{FT}, bio::HyperspectralLeafBiophysics{FT}, lwc::FT, x::FT, N::Int) where {FT}
-    (; K_ANT, K_BROWN, K_CAB, K_CAR_V, K_CAR_Z, K_CBC, K_H₂O, K_LMA, K_PRO) = lha;
-
-    # define the vectors
-    _sum_ki_xi = similar(K_CAB);
-    _k_cab = similar(K_CAB);
-    _k_car = similar(K_CAB);
-    _f_cab = similar(K_CAB);
-    _f_car = similar(K_CAB);
-    _τ_all = similar(K_CAB);
-
-    # compute the sum of absorption coefficients in one of the N sublayers of 1 layer (x of the total leaf thickness)
-    @. _sum_ki_xi = K_CAB   * bio.cab +                                 # chlorophyll absorption
-                    K_CAR_V * bio.car * (1 - bio.f_zeax) +              # violaxanthin carotenoid absorption
-                    K_CAR_Z * bio.car * bio.f_zeax +                    # zeaxanthin carotenoid absorption
-                    K_ANT   * bio.ant +                                 # anthocynanin absorption absorption
-                    K_BROWN * bio.brown +                               # brown pigments
-                    K_H₂O   * (lwc * M_H₂O(FT) / ρ_H₂O(FT) * 100) +     # water absorption
-                    K_CBC   * bio.cbc +                                 # carbon-based constituents absorption
-                    K_PRO   * bio.pro +                                 # protein absorption
-                    K_LMA   * (bio.lma - bio.cbc - bio.pro);            # dry mass absorption (if some remained)
-    @. _k_cab = K_CAB * bio.cab;
-    @. _k_car = K_CAR_V * bio.car * (1 - bio.f_zeax) + K_CAR_Z * bio.car * bio.f_zeax;
-
-    # compute the absorption ratios from cab and car
-    @. _f_cab = _k_cab / _sum_ki_xi;
-    @. _f_car = _k_car / _sum_ki_xi;
-
-    #_sum_ki_xi .*= x / N;
-    _sum_ki_xi .*= x;
-
-    # this is the case when light penetrate with an angle (integrated over all angles, without accounting for F_CELL)
-    @. _τ_all = (1 - _sum_ki_xi) * exp(-_sum_ki_xi) + _sum_ki_xi^2 * expint(_sum_ki_xi + eps(FT));
-
-    return _τ_all .^ (FT(1) / N), _f_cab, _f_car
-end;
-
-
-#######################################################################################################################################################################################################
-#
 # Changes to this function
 # General
 #     2023-Sep-15: add function to compute the absorption ratio from chlorophyll a and b
@@ -161,7 +102,7 @@ Return the fraction of chlorophyll a and b absorption in the sublayer, given
 - `k_pro` protein absorption coefficient
 - `lwc` leaf water content
 - `x` proportion of the layer of the whole leaf
-- `N` number of sublayers of the whole layer
+- `N` number of sublayers of each layer
 
 """
 function sublayer_τ(bios::HyperLeafBioState{FT}, k_ant::FT, k_brown::FT, k_cab::FT, k_car_v::FT, k_car_z::FT, k_cbc::FT, k_H₂O::FT, k_lma::FT, k_pro::FT, lwc::FT, x::FT, N::Int) where {FT}
@@ -190,22 +131,22 @@ end;
 #######################################################################################################################################################################################################
 """
 
-    leaf_sublayer_absorptions!(config::SPACConfiguration{FT}, bio::HyperLeafBio{FT}, lwc::FT; N::Int = 10) where {FT}
-    leaf_sublayer_absorptions!(lha::HyperspectralAbsorption{FT}, bio::HyperLeafBio{FT}, lwc::FT; N::Int = 10) where {FT}
+    leaf_sublayer_absorptions!(config::SPACConfiguration{FT}, bio::HyperLeafBio{FT}, lwc::FT, N::Int) where {FT}
+    leaf_sublayer_absorptions!(lha::HyperspectralAbsorption{FT}, bio::HyperLeafBio{FT}, lwc::FT, N::Int) where {FT}
 
 Update the sublayer absorption and transmittance within `bio`, given
 - `config` SPAC configuration
 - `bio` HyperLeafBio struct
 - `lwc` leaf water content
-- `N` number of sublayers of the whole layer
+- `N` number of sublayers of each layer
 - `lha` HyperspectralAbsorption struct
 
 """
 function leaf_sublayer_absorptions! end;
 
-leaf_sublayer_absorptions!(config::SPACConfiguration{FT}, bio::HyperLeafBio{FT}, lwc::FT; N::Int = 10) where {FT} = leaf_sublayer_absorptions!(config.lha, bio, lwc; N = N);
+leaf_sublayer_absorptions!(config::SPACConfiguration{FT}, bio::HyperLeafBio{FT}, lwc::FT, N::Int) where {FT} = leaf_sublayer_absorptions!(config.lha, bio, lwc, N);
 
-leaf_sublayer_absorptions!(lha::HyperspectralAbsorption{FT}, bio::HyperLeafBio{FT}, lwc::FT; N::Int = 10) where {FT} = (
+leaf_sublayer_absorptions!(lha::HyperspectralAbsorption{FT}, bio::HyperLeafBio{FT}, lwc::FT, N::Int) where {FT} = (
     (; K_ANT, K_BROWN, K_CAB, K_CAR_V, K_CAR_Z, K_CBC, K_H₂O, K_LMA, K_PRO) = lha;
 
     x = 1 / bio.state.meso_n;
