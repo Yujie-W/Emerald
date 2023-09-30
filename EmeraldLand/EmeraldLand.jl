f5f704f0505e6ad325fc309a62df8c5919786c43
Yujie Wang <jesiner@gmail.com>
2023-09-30 14:56:46 -0700

    rename folders in EmeraldLand

--- EmeraldLand/EmeraldLand.jl
+++ EmeraldLand/EmeraldLand.jl
@@ -5,19 +5,19 @@ using ..EmeraldMath
 
 
 include("Constant.jl");
-include("namespace/Namespace.jl");
+include("Namespace/Namespace.jl");
 
 include("EarthGeometry.jl");
 include("Optics.jl");
 include("PhysicalChemistry.jl");
 
-include("optics/LeafOptics.jl");
-include("radiation/CanopyOptics.jl");
-include("photosynthesis/Photosynthesis.jl");
-include("soil/SoilHydraulics.jl");
+include("LeafOptics/LeafOptics.jl");
+include("CanopyOptics/CanopyOptics.jl");
+include("Photosynthesis/Photosynthesis.jl");
+include("SoilHydraulics/SoilHydraulics.jl");
 include("PlantHydraulics/PlantHydraulics.jl");
-include("stomata/StomatalModels.jl");
-include("spac/SPAC.jl");
+include("StomatalModels/StomatalModels.jl");
+include("SPAC/SPAC.jl");
 
 
 end
