6c67eed20971ae3505c43a45268be35437585ba2
Yujie Wang <jesiner@gmail.com>
2024-01-25 14:29:21 -0800

    add v6 spectra with somehow calibrated vlues from literature

--- EmeraldLand/Namespace/Namespace.jl
+++ EmeraldLand/Namespace/Namespace.jl
@@ -17,10 +17,10 @@ using ..EmeraldPhysics.Constant: CP_D_MOL, CP_L, CP_L_MOL, CP_V_MOL, GAS_R, GRAV
 
 
 # Please do not use V1/V2/V3 files here as they do not contain the Phi_PSI and Phi_PSII variables
-const LAND_2017     = artifact"land_model_spectrum_V5" * "/clima_land_spectra_2017.nc";
-const LAND_2021     = artifact"land_model_spectrum_V5" * "/clima_land_spectra_2021.nc";
-const LAND_2017_1NM = artifact"land_model_spectrum_V5" * "/clima_land_spectra_1nm_2017.nc";
-const LAND_2021_1NM = artifact"land_model_spectrum_V5" * "/clima_land_spectra_1nm_2021.nc";
+const LAND_2017     = artifact"land_model_spectrum_V6" * "/clima_land_spectra_2017.nc";
+const LAND_2021     = artifact"land_model_spectrum_V6" * "/clima_land_spectra_2021.nc";
+const LAND_2017_1NM = artifact"land_model_spectrum_V6" * "/clima_land_spectra_1nm_2017.nc";
+const LAND_2021_1NM = artifact"land_model_spectrum_V6" * "/clima_land_spectra_1nm_2021.nc";
 const SOIL_TEXT     = read_csv("$(@__DIR__)/../../data/SOIL-TEXTURE.csv");
 
 
