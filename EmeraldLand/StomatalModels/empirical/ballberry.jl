61bdec1b95eaa28a5f5ac2e42fb598d58de8fa41
Yujie Wang <jesiner@gmail.com>
2025-06-05 07:55:00 +0800

    StomatalModels: scale both G0 and G1 terms with beta factor

--- EmeraldLand/StomatalModels/empirical/ballberry.jl
+++ EmeraldLand/StomatalModels/empirical/ballberry.jl
@@ -23,5 +23,5 @@ function empirical_equation end;
 empirical_equation(sm::BallBerrySM{FT}, leaf::CanopyLayer{FT}, air::AirLayer{FT}; β::FT = FT(1)) where {FT} = (
     (; G0, G1) = sm;
 
-    return G0 .+ β .* G1 .* air.s_aux.ps[3] ./ saturation_vapor_pressure(air.s_aux.t) .* leaf.flux.auxil.a_n * FT(1e-6) ./ leaf.flux.auxil.p_CO₂_s .* air.state.p_air
+    return (G0 .+ G1 .* air.s_aux.ps[3] ./ saturation_vapor_pressure(air.s_aux.t) .* leaf.flux.auxil.a_n * FT(1e-6) ./ leaf.flux.auxil.p_CO₂_s .* air.state.p_air) .* β
 );
