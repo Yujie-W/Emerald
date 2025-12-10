dbdcc7a7866a0d264cfabf09afc8bd14117af5c6
Yujie <jesiner@gmail.com>
2025-12-10 08:00:11 +0800

    use dynamic lai layer stratety by default: min 10, max Int(ceil(10*lai))

--- src/Land/initialize-spac.jl
+++ src/Land/initialize-spac.jl
@@ -1,20 +1,29 @@
 """
 
-    site_spac(config::SPACConfig{FT}, gmd::Dict{String,Any}) where {FT}
+    site_spac(config::SPACConfig{FT}, gmd::Dict{String,Any}; lai_layer_strategy::Bool = true) where {FT}
 
 Create a un-initialized SPAC using the data from a grid (CHL, VCMAX25, LAI, and CI are not prescribed as these changes with time), given
 - `config` Configurations for SPAC
 - `gmd` Dictionary of GriddingMachine data in a grid
+- `lai_layer_strategy` Whether to use LAI-based air layer strategy (default: true)
 
 """
-function site_spac(config::SPACConfig{FT}, gmd::Dict{String,Any}) where {FT}
+function site_spac(config::SPACConfig{FT}, gmd::Dict{String,Any}; lai_layer_strategy::Bool = true) where {FT}
+    # compute air layer bounds based on maximum LAI
+    zc = max(FT(0.05), gmd["CANOPY_HEIGHT"]);
+    air_bounds = if lai_layer_strategy
+        n_layer = max(10, Int(ceil(maximum(gmd["LAI"]) * 10)));
+        collect(0:(2n_layer+2)) * zc / (2n_layer)
+    else
+        collect(0:21) * zc / 20
+    end;
+
     #
     # TODO: add support to C4 photosynthesis
     #
-    zc = max(FT(0.05), gmd["CANOPY_HEIGHT"]);
     spac = BulkSPAC(
                 config;
-                air_bounds = collect(0:21) * zc / 20,
+                air_bounds = air_bounds,
                 elevation = gmd["ELEVATION"],
                 latitude = gmd["LATITUDE"],
                 longitude = gmd["LONGITUDE"],
