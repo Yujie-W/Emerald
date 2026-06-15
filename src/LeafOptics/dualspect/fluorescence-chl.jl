af3929a744c1a97a984704feb438000ced3fab87
Yujie <jesiner@gmail.com>
2026-06-15 16:08:12 +0800

    seems that e_net and sif_chl is not conserved...

--- src/LeafOptics/dualspect/fluorescence-chl.jl
+++ src/LeafOptics/dualspect/fluorescence-chl.jl
@@ -18,12 +18,14 @@ leaf_sif_matrices_chl!(config::SPACConfig{FT}, bio::LeafBio{FT}, cache::SPACCach
         ϕ .*= factor;
 
         # read in the values from the auxiliary variables
-        vec_b = view(bio.auxil.mat_b_chl, :, i);
-        vec_f = view(bio.auxil.mat_f_chl, :, i);
+        vec_b = view(bio.auxil.mat_b, :, i);
+        vec_f = view(bio.auxil.mat_f, :, i);
+        vec_b_chl = view(bio.auxil.mat_b_chl, :, i);
+        vec_f_chl = view(bio.auxil.mat_f_chl, :, i);
 
         # compute the total absorbed radiation
-        vec_b .= bio.auxil.α_leaf[ii] * bio.auxil.f_sife[ii] / 2 .* ϕ;
-        vec_f .= bio.auxil.α_leaf[ii] * bio.auxil.f_sife[ii] / 2 .* ϕ;
+        vec_b_chl .= bio.auxil.α_leaf[ii] * bio.auxil.f_sife[ii] .* ϕ .* vec_b ./ (vec_b .+ vec_f);
+        vec_f_chl .= bio.auxil.α_leaf[ii] * bio.auxil.f_sife[ii] .* ϕ .* vec_f ./ (vec_b .+ vec_f);
     end;
 
     # compute the mean and mean diff of mat_b_chl and mat_f_chl
--- test/debug/SIF-escape.jl
+++ test/debug/SIF-escape.jl
@@ -2,13 +2,14 @@ using DataFrames
 using Emerald
 using Emerald.Namespace
 using Emerald.SPAC
+using PkgUtility.UniversalConstants: energy_to_photon
 
 
 FT = Float64;
 
 config = Emerald.Namespace.SPACConfig(FT);
-config.METHODS.CANOPY_RT_METHOD = Namespace.CanopyRTSCOPE();
-#config.METHODS.CANOPY_RT_METHOD = Namespace.CanopyRTEmerald();
+#config.METHODS.CANOPY_RT_METHOD = Namespace.CanopyRTSCOPE();
+config.METHODS.CANOPY_RT_METHOD = Namespace.CanopyRTEmerald();
 
 lai = 3;
 
@@ -26,7 +27,16 @@ SPAC.initialize_spac!(config, spac);
 SPAC.spac!(config, spac, 1);
 
 
-sif_chl = spac.canopy.sun_geometry.auxil.e_sif_chl;
+e_net = (spac.canopy.sun_geometry.auxil.e_net_dif .+ spac.canopy.sun_geometry.auxil.e_net_dir)[config.CONSTANTS.SPECTRA.IΛ_SIFE, :];
+sif_chl = deepcopy(spac.canopy.sun_geometry.auxil.e_sif_chl);
 sif_leaf = spac.canopy.sun_geometry.auxil.e_sifꜛ_layer .+ spac.canopy.sun_geometry.auxil.e_sifꜜ_layer;
 
-esc_leaf = sif_leaf ./ sif_chl
+e_net_sum = [config.CONSTANTS.SPECTRA.ΔΛ_SIFE' * energy_to_photon.(config.CONSTANTS.SPECTRA.Λ_SIFE,eee) * 1e6 for eee in eachcol(e_net)];
+sif_chl_sum = [config.CONSTANTS.SPECTRA.ΔΛ_SIF' * energy_to_photon.(config.CONSTANTS.SPECTRA.Λ_SIF,sss) * 1e6 for sss in eachcol(sif_chl)];
+sif_leaf_sum = [config.CONSTANTS.SPECTRA.ΔΛ_SIF' * energy_to_photon.(config.CONSTANTS.SPECTRA.Λ_SIF,sss) * 1e6 for sss in eachcol(sif_leaf)];
+
+phi_layer = sif_chl_sum ./ e_net_sum;
+esc_leaf = sif_leaf_sum ./ sif_chl_sum;
+for i in eachindex(phi_layer)
+    @show i,phi_layer[i],esc_leaf[i];
+end;
